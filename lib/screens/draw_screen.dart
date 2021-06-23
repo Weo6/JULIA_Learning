9926bbd256e7a59e359ff59c51c168363fc26863
Weo6 <102196871+Weo6@users.noreply.github.com>
2021-06-23 19:34:26 +0000

    8.1.0

--- lib/screens/draw_screen.dart
+++ lib/screens/draw_screen.dart
@@ -2,3 +2,36 @@
 import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
+import 'package:mnistdigitrecognizer/models/prediction.dart';
+import 'package:mnistdigitrecognizer/screens/drawing_painter.dart';
+import 'package:mnistdigitrecognizer/screens/prediction_widget.dart';
+import 'package:mnistdigitrecognizer/services/recognizer.dart';
+import 'package:mnistdigitrecognizer/utils/constants.dart';
+
+class DrawScreen extends StatefulWidget {
+  @override
+  _DrawScreenState createState() => _DrawScreenState();
+}
+
+class _DrawScreenState extends State<DrawScreen> {
+  final _points = List<Offset>();
+  final _recognizer = Recognizer();
+  List<Prediction> _prediction;
+  bool initialize = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _initModel();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Digit Recognizer'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
