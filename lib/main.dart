d031918c6b38cc13d50e51f68f59126d5291bd67
Weo6 <102196871+Weo6@users.noreply.github.com>
2021-06-23 16:54:42 +0000

    added reset

--- lib/main.dart
+++ lib/main.dart
@@ -17,4 +17,6 @@ class MyApp extends StatelessWidget {
           color: Colors.black,
         ),
         floatingActionButtonTheme: FloatingActionButtonThemeData(
-          backgroundColor: Color
+          backgroundColor: Colors.black,
+        ),
+      
