1a9fc6fd9838fa06c6c5cc561138caef9874581a
Weo6 <102196871+Weo6@users.noreply.github.com>
2021-06-27 09:41:00 +0000

    Fix error message

--- lib/screens/prediction_widget.dart
+++ lib/screens/prediction_widget.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 import 'package:mnistdigitrecognizer/models/prediction.dart';
 
 class PredictionWidget extends StatelessWidget {
-  final Lis
+  final List<Prediction> predictions;
