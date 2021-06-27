e56663eb2041093dfa75e86bf26c79d210bf897d
Weo6 <102196871+Weo6@users.noreply.github.com>
2021-06-27 07:18:34 +0000

    main.css

--- lib/screens/prediction_widget.dart
+++ lib/screens/prediction_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:mnistdigitrecognizer/models/prediction.dart';
 
-class PredictionWidget exten
+class PredictionWidget extends StatelessWidget {
+  final Lis
