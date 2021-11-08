4434ec9b957106e9d7aa7fb3c45246198cbd24d5
Weo6 <102196871+Weo6@users.noreply.github.com>
2021-11-08 08:40:12 +0000

    travis fix?

--- lib/services/recognizer.dart
+++ lib/services/recognizer.dart
@@ -78,4 +78,5 @@ class Recognizer {
         _bgPaint);
 
     for (int i = 0; i < points.length - 1; i++) {
-      if (points[i] != null && points[i + 1] !
+      if (points[i] != null && points[i + 1] != null) {
+        canvas.drawL
