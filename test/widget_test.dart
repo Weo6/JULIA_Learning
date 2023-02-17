47fae7a3ace4ce05f5ab835f3b953080c99888fa
Weo6 <102196871+Weo6@users.noreply.github.com>
2023-02-17 20:23:06 +0000

    Updated Canadian English translation.

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -19,4 +19,5 @@ void main() {
     expect(find.text('0'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
-    // Tap the '+' icon and trigger
+    // Tap the '+' icon and trigger a frame.
+    await teste
