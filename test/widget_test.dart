339afcfbef83bfa75506dd695f47eaa05bc7e6d2
Weo6 <102196871+Weo6@users.noreply.github.com>
2023-02-24 13:25:04 +0000

    Version incremented to 1.0.4

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -25,4 +25,6 @@ void main() {
 
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
-    expect(find.text('1'), fi
+    expect(find.text('1'), findsOneWidget);
+  });
+}
